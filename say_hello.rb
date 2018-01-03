6e66d73b6324e11f6a5a25e41b72533a850e1932
A I <atbisrael@gmail.com>
2018-01-03 02:54:33 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,4 @@
 # Build your say_hello method here
-def say_hello(name)
- if (name){
+def say_hello(name = "Ruby Programmer")
    puts "Hello " + name + "!"
- } 
- : "Hello Ruby Programmer!"
- 
 end
